cceefa5b409e9cf5258caa3b3d441691c306e40f
hechtoid <hechtsp@gmail.com>
2020-02-15 02:50:34 -0800

    first commit

---
+++ precache-manifest.0de61b27c291927b286df1ad400e7461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817fa5837a5784cdb44b788251b820b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd00d91b7c3b4648c675",
+    "url": "/static/css/main.e7e432c7.chunk.css"
+  },
+  {
+    "revision": "140dec6efa408c324ecc",
+    "url": "/static/js/2.1b0171b6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1b0171b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd00d91b7c3b4648c675",
+    "url": "/static/js/main.614ca5d9.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
